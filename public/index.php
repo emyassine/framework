a57f315bf4c0a21b37ad1f09c244fb403eb2a6b7
emyassine <info@numerimondes.com>
2026-08-20 20:08:31 +0100

    WIP

--- public/index.php
+++ public/index.php
@@ -1,12 +1,6 @@
 <?php declare(strict_types=1);
-define('START_REQUEST', hrtime(true));
-
-use Webkernel\Http\Request;
-
-if (file_exists($maint = __DIR__.'/../storage/maintenance.php')) {
-    require $maint;
-}
-
+define('START_REQUEST', hrtime(true)); use Webkernel\Http\Request;
+if (file_exists($maint = __DIR__.'/../storage/maintenance.php')) { require $maint; }
 // ---- Start WebApp -------------------------------------------------------------------------
 // Webkernel Web Application is ... and does ...
 (require __DIR__.'/../bootstrap/app.php')
