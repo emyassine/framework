c311d7d548ce51b91965f2bece1e5e89269f2e90
emyassine <info@numerimondes.com>
2026-08-23 16:53:24 +0100

    WIP

--- refactor/modules/Blog/routes.php
+++ refactor/modules/Blog/routes.php
@@ -5,4 +5,3 @@ use Webkernel\Route\Route;
 Route::get('/blog', static fn (): string => 'Blog index');
 Route::get('/blog/posts', static fn (): string => 'Blog posts list')->name('blog.posts.index');
 Route::get('/blog/posts/{id}', static fn (): string => 'Blog post detail')->name('blog.posts.show');
-Route::post('/blog/posts', static fn (): string => 'Create blog post')->name('blog.posts.store');
