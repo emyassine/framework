27b7353fae54719260d6000c8eb1bb3f0e900ce9
emyassine <info@numerimondes.com>
2026-08-23 17:22:34 +0100

    WIP

--- refactor/modules/Blog/routes.php
+++ refactor/modules/Blog/routes.php
@@ -2,6 +2,15 @@
 
 use Webkernel\Route\Route;
 
-Route::get('/blog', static fn (): string => 'Blog index');
-Route::get('/blog/posts', static fn (): string => 'Blog posts list')->name('blog.posts.index');
-Route::get('/blog/posts/{id}', static fn (): string => 'Blog post detail')->name('blog.posts.show');
+Route::group(['prefix' => '/blog'], static function () {
+
+    Route::get('/', static fn (): string => 'Blog index');
+
+    Route::get('/posts', static fn (): string => 'Blog posts list')->name('blog.posts.index');
+
+    Route::get('/posts/{id}', static fn (string $id): string =>
+        'Blog post ' . $id . ' detail in ' .
+        number_format((hrtime(true) - START_REQUEST) / 1e6, 2) . ' ms'
+    )->name('blog.posts.show');
+
+});
