1bcc402821906e2085183e722463bafe5af48f84
emyassine <info@numerimondes.com>
2026-08-29 19:11:46 +0100

    WIP

---
+++ refactor/x-webkernel/devtools/src/DevToolsProvider.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+//> This file is part of Webkernel.
+//> (c) 2025 - 2027 Numerimondes, El Moumen Yassine
+//> Yassine El Moumen <yassine@numerimondes.com> | <platform@webkernelphp.com>
+//> For the full copyright and license information, please view the LICENSE
+//> file that was distributed with this source code.
+
+namespace Webkernel\DevTools;
+use Webkernel\PlatformProvider;
+
+class DevToolsProvider extends PlatformProvider
+{
+
+}
