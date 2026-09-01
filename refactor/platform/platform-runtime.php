989595fef2c74355a6fe8ade61eaf72a9551cda7
emyassine <info@numerimondes.com>
2026-09-01 01:26:55 +0100

    WIP

--- refactor/platform/platform-runtime.php
+++ refactor/platform/platform-runtime.php
@@ -61,7 +61,7 @@ return array (
         array (
           'site_title' => '',
           'meta_description' => 'dsd',
-          'keywords' => 'dsds',
+          'keywords' => 'dsdsd',
         ),
         'es' => 
         array (
